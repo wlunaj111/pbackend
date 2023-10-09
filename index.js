e0a7dd3038ccceb38e0e7bf3b0492fb06d80cdc4
Wilber <wlunaj111@gmail.com>
2023-10-09 11:06:55 -0400

    testing deploy

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 import express from 'express';
-
+import pg from 'pg'
+import {config} from 'dotenv'
 // Crear una nueva aplicación Express
+
+config()
 const app = express();
+
+
+
+const pool = new pg.Pool({    
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
+    });
 
 // Definir un puerto para nuestro servidor
 const port = 3000 || process.env.PORT;
@@ -10,6 +20,18 @@ const port = 3000 || process.env.PORT;
 app.get('/', (req, res) => {
   res.send('¡Hola Mundo modificado');
 });
+
+app.get('/ping', async (req, res) => {
+
+    try {
+       const result = await pool.query('SELECT NOW()')
+       console.log(result)
+      } catch(err) {
+        console.log(err);
+        console.log(err.stack);
+      }    
+    
+  });
 
 // Iniciar el servidor
 app.listen(port, () => {
